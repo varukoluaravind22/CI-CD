6ea7efcf56d9dfd0404144ed708083fd3245dfed
varukoluaravind22 <varukoluaravind22@gmail.com>
2026-03-31 14:33:28 +0530

    changes done for correcting backend.tf

--- Jenkins-ec2-instance/backend.tf
+++ Jenkins-ec2-instance/backend.tf
@@ -1,21 +1,20 @@
 #create bucker 
 
-terraform {
-  backend "s3" {
+resource "aws_s3_bucket" "mybucket"{
     bucket = "Mys3bucketforstatefile"
     versioning{
         enabled = true 
     }
-    server_side_encrytion_configuration{
-        rule{
-            apply_server_side_encryption_by_default {
-                sse_algorithm     = "AES256"
-           }
-        }
-    }
-  }
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "mybucketenc" {
+         bucket = aws_s3_bucket.mybucket.id
 
+        rule {
+            apply_server_side_encryption_by_default {
+            sse_algorithm     = "AES256"
+            }
+        }
+}
 #create dynamo_db
 
 resource "aws_dynamodb_table" "statelock" {
