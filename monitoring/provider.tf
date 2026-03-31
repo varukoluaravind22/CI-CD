ba6d4188e81d88d40ead19baa0def5444134323c
varukoluaravind22 <varukoluaravind22@gmail.com>
2026-03-31 15:31:30 +0530

    backend is added in both monitoring and jenkins

--- monitoring/provider.tf
+++ monitoring/provider.tf
@@ -1,4 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket = "mys3bucketforstatefile-1226"
+    dynamodb_table = "state-lock"
+    key = "Global/monitoring/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+  }
+}
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
