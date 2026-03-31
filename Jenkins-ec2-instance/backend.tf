da9b73476ec8dd1e735677b71081c5f43de51681
varukoluaravind22 <varukoluaravind22@gmail.com>
2026-03-31 15:18:05 +0530

    version resource and encr resources is added

--- Jenkins-ec2-instance/backend.tf
+++ Jenkins-ec2-instance/backend.tf
@@ -2,13 +2,15 @@
 
 resource "aws_s3_bucket" "mybucket"{
     bucket = "mys3bucketforstatefile"
-    versioning{
-        enabled = true 
-    }
+}
+resource "aws_s3_bucket_versioning" "mybuckeversion" {
+  bucket = aws_s3_bucket.mybucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "mybucketenc" {
          bucket = aws_s3_bucket.mybucket.id
-
         rule {
             apply_server_side_encryption_by_default {
             sse_algorithm     = "AES256"
