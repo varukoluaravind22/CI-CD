ad6c03116ccf9d89c4f95113f3b81a82f73b5bc5
varukoluaravind22 <varukoluaravind22@gmail.com>
2026-04-03 12:09:55 +0530

    added

--- Jenkins-ec2-instance/backend.tf
+++ Jenkins-ec2-instance/backend.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "mybucketenc" {
 }
 #create dynamo_db
 
-resource "aws_dynamodb_table" "statelock" {
+resource "aws_dynamodb_table" "state-lock" {
   name = "state-lock"
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "LockID"
